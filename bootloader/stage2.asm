de20da213b7af48293c36a1ad999063bb489212d
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 18:16:45 +0200

    VESA: actually fill the screen with blue after mode set

--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -1,19 +1,14 @@
-; Curs OS - Stage 2 Bootloader (VESA Framebuffer TUI)
-; Early skeleton - sets a graphics mode and clears to blue
-; NOTE: Still 16-bit real mode. Full drawing + font comes next.
+; Curs OS - Stage 2 (VESA)
+; Sets a graphics mode and fills the screen with blue so we can see it works
 
 [bits 16]
 [org 0x8000]
 
-; ============================================================
-; VESA / VBE
-; ============================================================
+MODE_800x600x32  equ 0x115
+MODE_1024x768x32 equ 0x118
 
-MODE_800x600x32   equ 0x115
-MODE_1024x768x32  equ 0x118
-
-vbe_info_block    equ 0x6000
-mode_info_block   equ 0x6200
+vbe_info         equ 0x6000
+mode_info        equ 0x6200
 
 start:
     cli
@@ -24,43 +19,45 @@ start:
     mov sp, 0x7C00
     sti
 
-    call vesa_init
-    jc .fallback_text
+    call vesa_setup
+    jc no_vesa
 
-    ; VESA mode is set - framebuffer is ready
-    ; For now we just stop here so you can see the mode change
-    ; (screen should go graphical / blue-ish depending on card)
+    ; Mode is set. Now fill the entire framebuffer with blue.
+    call fill_blue
+
 .hang:
     hlt
     jmp .hang
 
-.fallback_text:
+no_vesa:
     mov ax, 0x0003
     int 0x10
-    mov si, msg_vesa_fail
-    call print_text
+    mov si, fail_msg
+    call print
 .hang2:
     hlt
     jmp .hang2
 
-; ============================================================
-; VESA Initialization
-; ============================================================
-
-vesa_init:
-    mov ax, 0x4F00
-    mov di, vbe_info_block
+; ------------------------------------------------------------
+; VESA setup
+; ------------------------------------------------------------
+vesa_setup:
+    ; Get controller info
+    mov di, vbe_info
     mov dword [di], 'VBE2'
+    mov ax, 0x4F00
     int 0x10
     cmp ax, 0x004F
     jne .fail
 
+    ; Try 800x600x32
     mov cx, MODE_800x600x32
-    call try_mode
+    call set_mode
     jnc .ok
 
+    ; Try 1024x768x32
     mov cx, MODE_1024x768x32
-    call try_mode
+    call set_mode
     jnc .ok
 
 .fail:
@@ -70,60 +67,80 @@ vesa_init:
     clc
     ret
 
-try_mode:
+; CX = mode
+set_mode:
     push cx
     mov ax, 0x4F01
-    mov di, mode_info_block
+    mov di, mode_info
     int 0x10
     pop cx
     cmp ax, 0x004F
     jne .no
 
-    mov ax, [mode_info_block]
-    test ax, 0x80
+    ; Must have linear FB (bit 7)
+    test word [mode_info], 0x80
     jz .no
 
-    mov al, [mode_info_block + 25]
-    cmp al, 24
+    ; Bits per pixel
+    cmp byte [mode_info+25], 24
     jb .no
 
-    mov ax, [mode_info_block + 16]
-    mov [fb_pitch], ax
-    mov ax, [mode_info_block + 18]
-    mov [fb_width], ax
-    mov ax, [mode_info_block + 20]
-    mov [fb_height], ax
-    mov eax, [mode_info_block + 40]
-    mov [fb_addr], eax
+    ; Save info
+    mov ax, [mode_info+16]
+    mov [pitch], ax
+    mov ax, [mode_info+18]
+    mov [width], ax
+    mov ax, [mode_info+20]
+    mov [height], ax
+    mov eax, [mode_info+40]
+    mov [fb], eax
 
-    mov ax, 0x4F02
+    ; Set the mode + linear FB flag
     mov bx, cx
     or bx, 0x4000
+    mov ax, 0x4F02
     int 0x10
     cmp ax, 0x004F
     jne .no
-
     clc
     ret
 .no:
     stc
     ret
 
-print_text:
+; ------------------------------------------------------------
+; Fill the whole screen with blue
+; ------------------------------------------------------------
+fill_blue:
+    mov eax, [fb]
+    mov ecx, [width]
+    imul ecx, [height]
+
+    ; Blue color (try a couple of common formats)
+    mov ebx, 0x000000AA
+
+.next_pixel:
+    a32 mov [eax], ebx
+    add eax, 4
+    dec ecx
+    jnz .next_pixel
+    ret
+
+print:
     lodsb
-    or al, al
+    test al, al
     jz .done
     mov ah, 0x0E
     int 0x10
-    jmp print_text
+    jmp print
 .done:
     ret
 
-msg_vesa_fail db "VESA not available", 0
+fail_msg db "No usable VESA mode found", 0
 
-fb_addr   dd 0
-fb_pitch  dw 0
-fb_width  dw 0
-fb_height dw 0
+fb      dd 0
+pitch    dw 0
+width    dw 0
+height   dw 0
 
-times 1024-($-$$) db 0
+times 2048-($-$$) db 0
