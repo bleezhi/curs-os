6074cd9357189e81c3590bd44bb07f9f174ad6e1
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 17:46:05 +0200

    Stage 1 now loads Stage 2 from disk and jumps to it

--- bootloader/stage1.asm
+++ bootloader/stage1.asm
@@ -1,6 +1,6 @@
 ; Curs OS - Stage 1 Boot Sector
-; Assembles to a classic 512-byte boot sector
-; Goal: load Stage 2 and jump to it
+; 512-byte BIOS boot sector
+; Loads Stage 2 and jumps to it
 
 [org 0x7C00]
 [bits 16]
@@ -14,30 +14,52 @@ start:
     mov sp, 0x7C00
     sti
 
-    ; Clear screen (simple)
+    ; Save boot drive (BIOS puts it in DL)
+    mov [boot_drive], dl
+
+    ; Clear screen
     mov ax, 0x0003
     int 0x10
 
-    ; Print a tiny banner so we know Stage 1 is alive
+    ; Tiny banner
     mov si, msg
-.print:
+    call print
+
+    ; Load Stage 2 from sector 2 onwards into 0x8000
+    ; (This assumes the image is built so Stage 2 starts at LBA 1)
+    mov ah, 0x02          ; read sectors
+    mov al, 16            ; number of sectors to read (8 KB for now)
+    mov ch, 0             ; cylinder
+    mov cl, 2             ; sector (1-based, sector 2)
+    mov dh, 0             ; head
+    mov dl, [boot_drive]
+    mov bx, 0x8000        ; destination
+    int 0x13
+    jc disk_error
+
+    ; Jump to Stage 2
+    jmp 0x0000:0x8000
+
+disk_error:
+    mov si, err_msg
+    call print
+.hang:
+    hlt
+    jmp .hang
+
+print:
     lodsb
     or al, al
     jz .done
     mov ah, 0x0E
     int 0x10
-    jmp .print
+    jmp print
 .done:
+    ret
 
-    ; TODO: Load Stage 2 from disk into memory
-    ; TODO: Jump to Stage 2
+boot_drive db 0
+msg      db "Curs Stage 1 - loading TUI...", 0
+err_msg  db " Disk error!", 0
 
-.hang:
-    hlt
-    jmp .hang
-
-msg db "Curs Stage 1", 0
-
-; Boot signature
 times 510-($-$$) db 0
 dw 0xAA55
