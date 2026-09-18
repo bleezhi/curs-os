32c4567695ce2cc310c41a6452e41335f702afdd
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 17:57:01 +0200

    Improve Stage 1: better error reporting and disk reset

--- bootloader/stage1.asm
+++ bootloader/stage1.asm
@@ -1,6 +1,5 @@
 ; Curs OS - Stage 1 Boot Sector
-; 512-byte BIOS boot sector
-; Loads Stage 2 and jumps to it
+; More robust version with better error reporting
 
 [org 0x7C00]
 [bits 16]
@@ -14,35 +13,43 @@ start:
     mov sp, 0x7C00
     sti
 
-    ; Save boot drive (BIOS puts it in DL)
+    ; Save boot drive
     mov [boot_drive], dl
 
-    ; Clear screen
+    ; Set text mode and clear
     mov ax, 0x0003
     int 0x10
 
-    ; Tiny banner
-    mov si, msg
+    mov si, msg_start
     call print
 
-    ; Load Stage 2 from sector 2 onwards into 0x8000
-    ; (This assumes the image is built so Stage 2 starts at LBA 1)
-    mov ah, 0x02          ; read sectors
-    mov al, 16            ; number of sectors to read (8 KB for now)
-    mov ch, 0             ; cylinder
-    mov cl, 2             ; sector (1-based, sector 2)
-    mov dh, 0             ; head
+    ; Reset disk system first
+    xor ah, ah
     mov dl, [boot_drive]
-    mov bx, 0x8000        ; destination
+    int 0x13
+
+    ; Try to read Stage 2 (16 sectors starting at sector 2)
+    mov ax, 0x0200 + 16     ; AH=02 read, AL=16 sectors
+    mov ch, 0               ; cylinder 0
+    mov cl, 2               ; starting sector 2
+    mov dh, 0               ; head 0
+    mov dl, [boot_drive]
+    mov bx, 0x8000          ; load address
     int 0x13
     jc disk_error
+
+    mov si, msg_ok
+    call print
 
     ; Jump to Stage 2
     jmp 0x0000:0x8000
 
 disk_error:
-    mov si, err_msg
+    mov si, msg_err
     call print
+    ; Print the error code in AH
+    mov al, ah
+    call print_hex_byte
 .hang:
     hlt
     jmp .hang
@@ -52,14 +59,38 @@ print:
     or al, al
     jz .done
     mov ah, 0x0E
+    mov bh, 0
     int 0x10
     jmp print
 .done:
     ret
 
+; Print AL as two hex digits
+print_hex_byte:
+    push ax
+    shr al, 4
+    call .nibble
+    pop ax
+    and al, 0x0F
+    call .nibble
+    ret
+.nibble:
+    and al, 0x0F
+    cmp al, 10
+    jl .digit
+    add al, 'A' - 10
+    jmp .out
+.digit:
+    add al, '0'
+.out:
+    mov ah, 0x0E
+    int 0x10
+    ret
+
 boot_drive db 0
-msg      db "Curs Stage 1 - loading TUI...", 0
-err_msg  db " Disk error!", 0
+msg_start db "Curs Stage 1...", 0
+msg_ok    db " OK, jumping to TUI", 13, 10, 0
+msg_err   db " Disk error AH=", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
