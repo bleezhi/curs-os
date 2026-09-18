28af8fbf46dac340e3ce8045a39002e4b24e3d48
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 18:11:07 +0200

    Start VESA framebuffer path for graphical TUI (early skeleton)

--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -1,333 +1,129 @@
-; Curs OS - Stage 2 Bootloader TUI
-; Debian-style dialog with custom colors
+; Curs OS - Stage 2 Bootloader (VESA Framebuffer TUI)
+; Early skeleton - sets a graphics mode and clears to blue
+; NOTE: Still 16-bit real mode. Full drawing + font comes next.
 
 [bits 16]
 [org 0x8000]
 
-VGA_SEG      equ 0xB800
+; ============================================================
+; VESA / VBE
+; ============================================================
 
-; Color attributes (bg << 4 | fg)
-; 0=black 1=blue 2=green 3=cyan 4=red 5=magenta 6=brown 7=lightgrey
-; 8=darkgrey 9=lightblue ... 15=brightwhite
+MODE_800x600x32   equ 0x115
+MODE_1024x768x32  equ 0x118
 
-ATTR_BG      equ 0x17      ; light grey on blue   (whole screen background)
-ATTR_BOX     equ 0x7F      ; bright white on light grey  (box border - gray-white)
-ATTR_INSIDE  equ 0x1F      ; bright white on blue        (inside box / selection window)
-ATTR_TITLE   equ 0x1F      ; bright white on blue
-ATTR_SELECT  equ 0x40      ; black on red                (selected - red "outline" feel)
-ATTR_NORMAL  equ 0x17      ; light grey on blue          (unselected - gray text on low blue)
-ATTR_HINT    equ 0x1E      ; yellow on blue
-
-NUM_ITEMS    equ 4
-
-; Box geometry
-BOX_TOP      equ 5
-BOX_LEFT     equ 18
-BOX_WIDTH    equ 44
-BOX_HEIGHT   equ 14
+vbe_info_block    equ 0x6000
+mode_info_block   equ 0x6200
 
 start:
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
+    call vesa_init
+    jc .fallback_text
+
+    ; VESA mode is set - framebuffer is ready
+    ; For now we just stop here so you can see the mode change
+    ; (screen should go graphical / blue-ish depending on card)
+.hang:
+    hlt
+    jmp .hang
+
+.fallback_text:
     mov ax, 0x0003
     int 0x10
+    mov si, msg_vesa_fail
+    call print_text
+.hang2:
+    hlt
+    jmp .hang2
 
-    ; Hide cursor
-    mov ah, 0x01
-    mov cx, 0x2000
+; ============================================================
+; VESA Initialization
+; ============================================================
+
+vesa_init:
+    mov ax, 0x4F00
+    mov di, vbe_info_block
+    mov dword [di], 'VBE2'
     int 0x10
+    cmp ax, 0x004F
+    jne .fail
 
-    mov byte [selected], 0
-    call draw_all
+    mov cx, MODE_800x600x32
+    call try_mode
+    jnc .ok
 
-.main_loop:
-    call wait_key
-    cmp ah, 0x48            ; Up
-    je .up
-    cmp ah, 0x50            ; Down
-    je .down
-    cmp al, 0x0D            ; Enter
-    je .enter
-    cmp al, 0x1B            ; Esc
-    je do_reboot
-    jmp .main_loop
+    mov cx, MODE_1024x768x32
+    call try_mode
+    jnc .ok
 
-.up:
-    cmp byte [selected], 0
-    je .main_loop
-    dec byte [selected]
-    call draw_menu_only
-    jmp .main_loop
-
-.down:
-    cmp byte [selected], NUM_ITEMS-1
-    je .main_loop
-    inc byte [selected]
-    call draw_menu_only
-    jmp .main_loop
-
-.enter:
-    mov al, [selected]
-    cmp al, 0
-    je boot_curs
-    cmp al, 1
-    je boot_debug
-    cmp al, 2
-    je do_reboot
-    cmp al, 3
-    je show_about
-    jmp .main_loop
-
-; ======================== DRAWING ========================
-
-draw_all:
-    call clear_screen
-    call draw_box
-    call draw_title
-    call draw_menu_only
-    call draw_hint
+.fail:
+    stc
+    ret
+.ok:
+    clc
     ret
 
-clear_screen:
-    push es
-    mov ax, VGA_SEG
-    mov es, ax
-    xor di, di
-    mov cx, 80*25
-    mov ax, (ATTR_BG << 8) | ' '
-    rep stosw
-    pop es
-    ret
-
-draw_box:
-    push es
-    mov ax, VGA_SEG
-    mov es, ax
-
-    ; Top border (gray-white)
-    mov di, BOX_TOP*160 + BOX_LEFT*2
-    mov ax, (ATTR_BOX << 8) | 0xC9      ; ╔
-    stosw
-    mov cx, BOX_WIDTH-2
-    mov ax, (ATTR_BOX << 8) | 0xCD      ; ═
-.top:
-    stosw
-    loop .top
-    mov ax, (ATTR_BOX << 8) | 0xBB      ; ╗
-    stosw
-
-    ; Sides + blue inside fill
-    mov cx, BOX_HEIGHT-2
-    mov bx, 1
-.sides:
+try_mode:
     push cx
-    mov ax, BOX_TOP
-    add ax, bx
-    imul ax, 160
-    add ax, BOX_LEFT*2
-    mov di, ax
-
-    mov ax, (ATTR_BOX << 8) | 0xBA      ; ║
-    stosw
-
-    push cx
-    mov cx, BOX_WIDTH-2
-    mov ax, (ATTR_INSIDE << 8) | ' '    ; blue selection window
-.fill:
-    stosw
-    loop .fill
+    mov ax, 0x4F01
+    mov di, mode_info_block
+    int 0x10
     pop cx
+    cmp ax, 0x004F
+    jne .no
 
-    mov ax, (ATTR_BOX << 8) | 0xBA
-    stosw
+    mov ax, [mode_info_block]
+    test ax, 0x80
+    jz .no
 
-    inc bx
-    pop cx
-    loop .sides
+    mov al, [mode_info_block + 25]
+    cmp al, 24
+    jb .no
 
-    ; Bottom border
-    mov ax, BOX_TOP + BOX_HEIGHT - 1
-    imul ax, 160
-    add ax, BOX_LEFT*2
-    mov di, ax
-    mov ax, (ATTR_BOX << 8) | 0xC8      ; ╚
-    stosw
-    mov cx, BOX_WIDTH-2
-    mov ax, (ATTR_BOX << 8) | 0xCD
-.bot:
-    stosw
-    loop .bot
-    mov ax, (ATTR_BOX << 8) | 0xBC      ; ╝
-    stosw
+    mov ax, [mode_info_block + 16]
+    mov [fb_pitch], ax
+    mov ax, [mode_info_block + 18]
+    mov [fb_width], ax
+    mov ax, [mode_info_block + 20]
+    mov [fb_height], ax
+    mov eax, [mode_info_block + 40]
+    mov [fb_addr], eax
 
-    pop es
+    mov ax, 0x4F02
+    mov bx, cx
+    or bx, 0x4000
+    int 0x10
+    cmp ax, 0x004F
+    jne .no
+
+    clc
+    ret
+.no:
+    stc
     ret
 
-draw_title:
-    mov si, title_str
-    mov dh, BOX_TOP + 1
-    mov dl, BOX_LEFT + 12
-    mov bl, ATTR_TITLE
-    call print_at
-    ret
-
-draw_menu_only:
-    ; Item 0
-    mov si, item0
-    mov dh, BOX_TOP + 4
-    mov dl, BOX_LEFT + 4
-    mov bl, ATTR_NORMAL
-    cmp byte [selected], 0
-    jne .i0
-    mov bl, ATTR_SELECT
-.i0:
-    call print_at
-
-    ; Item 1
-    mov si, item1
-    mov dh, BOX_TOP + 6
-    mov dl, BOX_LEFT + 4
-    mov bl, ATTR_NORMAL
-    cmp byte [selected], 1
-    jne .i1
-    mov bl, ATTR_SELECT
-.i1:
-    call print_at
-
-    ; Item 2
-    mov si, item2
-    mov dh, BOX_TOP + 8
-    mov dl, BOX_LEFT + 4
-    mov bl, ATTR_NORMAL
-    cmp byte [selected], 2
-    jne .i2
-    mov bl, ATTR_SELECT
-.i2:
-    call print_at
-
-    ; Item 3
-    mov si, item3
-    mov dh, BOX_TOP + 10
-    mov dl, BOX_LEFT + 4
-    mov bl, ATTR_NORMAL
-    cmp byte [selected], 3
-    jne .i3
-    mov bl, ATTR_SELECT
-.i3:
-    call print_at
-    ret
-
-draw_hint:
-    mov si, hint_str
-    mov dh, BOX_TOP + BOX_HEIGHT - 2
-    mov dl, BOX_LEFT + 5
-    mov bl, ATTR_HINT
-    call print_at
-    ret
-
-print_at:
-    push es
-    push ax
-    push bx
-    push di
-
-    mov ax, VGA_SEG
-    mov es, ax
-
-    movzx ax, dh
-    imul ax, 160
-    movzx bx, dl
-    shl bx, 1
-    add ax, bx
-    mov di, ax
-
-.loop:
+print_text:
     lodsb
     or al, al
     jz .done
-    mov ah, bl
-    stosw
-    jmp .loop
+    mov ah, 0x0E
+    int 0x10
+    jmp print_text
 .done:
-    pop di
-    pop bx
-    pop ax
-    pop es
     ret
 
-wait_key:
-    xor ah, ah
-    int 0x16
-    ret
+msg_vesa_fail db "VESA not available", 0
 
-; ======================== ACTIONS ========================
+fb_addr   dd 0
+fb_pitch  dw 0
+fb_width  dw 0
+fb_height dw 0
 
-boot_curs:
-    call clear_screen
-    mov si, msg_boot
-    mov dh, 12
-    mov dl, 28
-    mov bl, ATTR_TITLE
-    call print_at
-    jmp $
-
-boot_debug:
-    call clear_screen
-    mov si, msg_debug
-    mov dh, 12
-    mov dl, 26
-    mov bl, ATTR_TITLE
-    call print_at
-    jmp $
-
-do_reboot:
-    db 0xEA
-    dw 0x0000
-    dw 0xFFFF
-
-show_about:
-    call clear_screen
-    call draw_box
-    mov si, about1
-    mov dh, BOX_TOP + 4
-    mov dl, BOX_LEFT + 14
-    mov bl, ATTR_TITLE
-    call print_at
-    mov si, about2
-    mov dh, BOX_TOP + 6
-    mov dl, BOX_LEFT + 4
-    mov bl, ATTR_NORMAL
-    call print_at
-    mov si, about3
-    mov dh, BOX_TOP + 8
-    mov dl, BOX_LEFT + 8
-    mov bl, ATTR_NORMAL
-    call print_at
-    mov si, about4
-    mov dh, BOX_TOP + 11
-    mov dl, BOX_LEFT + 8
-    mov bl, ATTR_HINT
-    call print_at
-
-    call wait_key
-    call draw_all
-    jmp start.main_loop
-
-; ======================== DATA ========================
-
-selected db 0
-
-title_str db " Curs Bootloader ", 0
-
-item0     db "  Boot Curs OS              ", 0
-item1     db "  Boot with Debug           ", 0
-item2     db "  Reboot                    ", 0
-item3     db "  About Curs                ", 0
-
-hint_str  db "Arrow keys to select  -  Enter to confirm", 0
-
-msg_boot  db "Loading Curs OS...", 0
-msg_debug db "Loading with debug...", 0
-
-about1    db "Curs OS", 0
-about2    db "Custom bootloader - no GRUB, no Limine", 0
-about3    db "Pure VGA text-mode TUI", 0
-about4    db "Press any key to return", 0
+times 1024-($-$$) db 0
